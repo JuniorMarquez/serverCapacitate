0a85e23b7d805ffb9f92438cc80a04fc140de131
root <root@sohigh.com.ve>
2017-06-26 14:11:47 +0000

    add userCapacitate

---
+++ api/controllers/UserCapacitateController.js
@@ -0,0 +1,11 @@
+/**
+ * UserCapacitateController
+ *
+ * @description :: Server-side logic for managing usercapacitates
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
